ca62967c50c897a8907594aaeef079915ff85bd6
henipatel8 <43182867+henipatel8@users.noreply.github.com>
2020-11-09 09:40:24 -0500

    changes in all files

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  "globDirectory": "D:\\sem 5\\practical applications\\pwa\\Ontario_Archaeology_Museum-AR-master\\Ontario_Archaeology_Museum-AR-master",
+  "globDirectory": "D:\\sem 5\\practical applications\\pwa\\henipatel8.github.io",
   "globPatterns": [
     "**/*.{yml,html,css,md,jpg,usdz,png,svg,JPG,xml,ico,js,gif,txt,json}"
   ],
-  "swDest": "D:/sem 5/practical applications/pwa/Ontario_Archaeology_Museum-AR-master/Ontario_Archaeology_Museum-AR-master/sw.js",
+  "swDest": "D:/sem 5/practical applications/pwa/henipatel8.github.io/sw.js",
   "maximumFileSizeToCacheInBytes": 50000000
 };
