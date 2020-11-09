28cda9a21ddc7f7455b4283dd84b843d6c22b0f4
henipatel8 <43182867+henipatel8@users.noreply.github.com>
2020-11-09 13:29:32 -0500

    video added

--- workbox-config.js
+++ workbox-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "globDirectory": "D:\\sem 5\\practical applications\\pwa\\henipatel8.github.io",
   "globPatterns": [
-    "**/*.{yml,html,css,md,jpg,usdz,png,svg,JPG,xml,ico,js,gif,txt,json}"
+    "**/*.{yml,html,css,md,jpg,usdz,png,svg,JPG,xml,ico,js,gif,txt,json,mp4}"
   ],
   "swDest": "D:/sem 5/practical applications/pwa/henipatel8.github.io/sw.js",
-  "maximumFileSizeToCacheInBytes": 50000000
+  "maximumFileSizeToCacheInBytes": 500000000
 };
